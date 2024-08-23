3b2f241032b640d290fcea48cceac891322d0575
rickydamarsaputra <retrocode.rc@gmail.com>
2024-08-23 20:38:16 +0700

    update: content website

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -46,7 +46,7 @@ export default function projects({ projects }) {
 					<div className="bg-gradient-to-tr from-blue-dark to-blue-light p-4">
 						<span className="text-sm">FEATURED PROJECT</span>
 						<h1 className="text-2xl font-semibold text-blue-neo">{featuredProject?.fields.title}</h1>
-						<p className="mt-2">{featuredProject?.fields.shortDescription.slice(0, 100) + "..."}</p>
+						<p className="mt-2">{featuredProject?.fields ? featuredProject?.fields.shortDescription.slice(0, 100) + "..." : null}</p>
 						<ul className="flex space-x-3 mt-4">
 							{featuredProject?.fields.type.map((type) => (
 								<li key={"key" + type}>
